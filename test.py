f05f0eac8beee25c479e09af02e0c6058a59fbf9
Matt <matthewltran@gmail.com>
2020-07-28 00:27:39 -0700

    removed loop to always check for messages

--- test.py
+++ test.py
@@ -8,13 +8,19 @@ print('hello World')
 logger = logging.getLogger('websockets')
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
+
+# websocket = await websockets.connect('ws://sim.smogon.com:8000/showdown/websocket')
+# print('Connection successful')
+# while True:
+#     message = await websocket.recv()
+#     print(message)
+#     print("<< {}".format(message))
+
 async def test():
     async with websockets.connect('ws://sim.smogon.com:8000/showdown/websocket') as websocket:
         print('Connection successful')
-        while True:
-            message = await websocket.recv()
-            print(message)
-            print("<< {}".format(message))
-            # await stringing(websocket, message)
+        message = await websocket.recv()
+        print(message)
+        print("<< {}".format(message))
 
-asyncio.run(test())
+asyncio.get_event_loop().run_until_complete(test())
